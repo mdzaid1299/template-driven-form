8c27ed03507a1c2d272583beef34e4ca051cd69e
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-01-22 23:57:51 +0530

    initial

--- template driven form/tempform/src/app/app.component.ts
+++ template driven form/tempform/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tempform';
+  topics = ["c" , "c++", "java" ];
+// binding html 
+userModel = new User ("ting", "t@ing.pu", "7245627255" , "c" , "morning")
+
 }
---
+++ template driven form/tempform/src/app/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    constructor(username: string,email: string,phone :string , interest:string , time:string){
+
+    }
+}
