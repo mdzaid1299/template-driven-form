079c93f56f1fba647f75e169e75d357618eb3961
Md. Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-01-27 19:38:51 +0530

    ini

--- template driven form/tempform/src/app/app.component.ts
+++ template driven form/tempform/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
-import { NgForm } from ;
+import { NgForm } from  '@angular/forms' ;
 
 
 @Component({
@@ -12,11 +12,13 @@ export class AppComponent {
   title = 'tempform';
   topics = ["c" , "c++", "java" ];
 // binding html 
+submitted = false;
+  user: any;
 userModel = new User ("ting", "t@ing.pu", "7245627255" , "c" , "morning")
 save(){
   alert("submitted")
 }
-onSubmit(userForm:Ngform){
+onSubmit(userForm:NgForm){
   this.submitted = true;
 }
 }
